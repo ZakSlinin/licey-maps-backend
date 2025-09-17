3604b634eded7c68f16f578318abe43fb6aba69b
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-09-17 10:04:51 +0300

    feat: nav_point_repository

--- nav-point-service/internal/repository/nav_point_repository.go
+++ nav-point-service/internal/repository/nav_point_repository.go
@@ -10,9 +10,9 @@ type NavPointRepository struct{ db *sqlx.DB }
 func NewNavPointRepository(db *sqlx.DB) *NavPointRepository { return &NavPointRepository{db: db} }
 
 func (r *NavPointRepository) CreateNavPoint(navPoint *model.NavPoint) (string, error) {
-	query := `INSERT INTO navPoint (id, orientation, room, type, floor) VALUES ($1, $2, $3, $4, $5) RETURNING id`
+	query := `INSERT INTO navPoint (id, nav_point_id, orientation, room, type, floor) VALUES ($1, $2, $3, $4, $5) RETURNING nav_point_id`
 
-	_, err := r.db.Exec(query, navPoint.ID, navPoint.Orientation, navPoint.Room, navPoint.Type, navPoint.Floor)
+	_, err := r.db.Exec(query, navPoint.ID, navPoint.NavPointID, navPoint.Orientation, navPoint.Room, navPoint.Type, navPoint.Floor)
 	if err != nil {
 		return "", err
 	}
@@ -20,6 +20,14 @@ func (r *NavPointRepository) CreateNavPoint(navPoint *model.NavPoint) (string, e
 	return navPoint.Room, nil
 }
 
-func (r *NavPointRepository) GetNavPointByID(id string) (*model.NavPoint, error) {
+func (r *NavPointRepository) GetNavPointByNavPointID(navPointId string) ([]model.NavPoint, error) {
+	var navPoints []model.NavPoint
+	query := `SELECT * FROM navPoint WHERE nav_point_id = $1`
 
+	_, err := r.db.Exec(query, navPointId)
+	if err != nil {
+		return nil, err
+	}
+
+	return navPoints, nil
 }
--- nav-point-service/internal/service/nav_point_service.go
+++ nav-point-service/internal/service/nav_point_service.go
@@ -1,1 +1,1 @@
-package servic
+package service
