de6adc6b284f5bd5ab4295450f6ba506529513b1
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-09-22 13:16:53 +0300

    update: point.go

--- point-service/internal/model/point.go
+++ point-service/internal/model/point.go
@@ -1,8 +1,9 @@
 package model
 
 type Point struct {
-	ID       int      `json:"id" db:"id"`
-	Name     string   `json:"name" db:"name"`
-	Env      []string `json:"env" db:"env"`
-	NavPoint int      `json:"nav_point" db:"nav_point"`
+	ID        int      `json:"id" db:"id"`
+	Name      string   `json:"name" db:"name"`
+	Env       []string `json:"env" db:"env"`
+	NavPoint  int      `json:"nav_point" db:"nav_point"`
+	Neighbors []int    `json:"neighbors" db:"neighbors"` // id соседних точек
 }
