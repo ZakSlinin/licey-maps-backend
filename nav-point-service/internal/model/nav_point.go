8e8bec1e608b72c1d6f728f9010496bd206e3b61
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-09-22 13:18:01 +0300

    update: nav_point.go

--- nav-point-service/internal/model/nav_point.go
+++ nav-point-service/internal/model/nav_point.go
@@ -3,6 +3,7 @@ package model
 type NavPoint struct {
 	ID          int    `json:"id" db:"id"`
 	Orientation string `json:"orientation" db:"orientation"`
+	Neighbours  []int  `json:"neighbours" db:"neighbours"`
 	Room        string `json:"room" db:"room"`
 	Type        string `json:"type" db:"type"`
 	Floor       int    `json:"floor" db:"floor"`
